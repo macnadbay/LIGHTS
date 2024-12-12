16ebb729a606b0d2e3f7a9399db14a7776594b63
Macnadbay <macnadbay@gmail.com>
2024-12-11 23:50:35 -0300

    bootstrap

--- SCRIPT.js
+++ SCRIPT.js
@@ -12,8 +12,13 @@ outputazul.innerHTML = sliderazul.value;
 
 const cor_nova = document.getElementById('cor-nova');
 const cor_atual = document.getElementById('cor-atual');
+let cor;
+let red, green, blue;
 
 const checkbox = document.getElementById('real_time');
+
+
+checkbox.checked = true;
   checkbox.addEventListener('change', () => {
     if (checkbox.checked) {
       console.log('Checkbox marcado!');
@@ -27,17 +32,19 @@ const checkbox = document.getElementById('real_time');
     const regex = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/;
     // Executa a regex na string fornecida
     const resultado = rgb.match(regex);
-    const r = parseInt(resultado[1]);
-    const g = parseInt(resultado[2]);
-    const b = parseInt(resultado[3]);
-    slidervermelho.value = 100*r/255;
-    outputvermelho.textContent = Math.round(100*r/255);
-    sliderverde.value = 100*g/255;
-    outputverde.textContent = Math.round(100*g/255);
-    sliderazul.value = 100*b/255;
-    outputazul.textContent = Math.round(100*b/255);
-    console.log(`Você selecionou: ${r},${g},${b}`);
-    cor = `rgb(${red}, ${green}, ${blue})`;
+    red = parseInt(resultado[1]);
+    green = parseInt(resultado[2]);
+    blue = parseInt(resultado[3]);
+    slidervermelho.value = 100*red/255;
+    outputvermelho.textContent = Math.round(100*red/255);
+    sliderverde.value = 100*green/255;
+    outputverde.textContent = Math.round(100*green/255);
+    sliderazul.value = 100*blue/255;
+    outputazul.textContent = Math.round(100*blue/255);
+    muda_cor_nova();
+    //console.log(`Você selecionou: ${red},${green},${blue}`);
+    //cor = `rgb(${r}, ${g}, ${b})`;
+    //console.log(`cor = ${cor}`)
   }
 
   const levels= [255,127,0];
@@ -68,13 +75,13 @@ const checkbox = document.getElementById('real_time');
         //label.style.backgroundColor = color;
 
         const span = document.createElement("span");
-        span.style.aspectRatio = "1/1";
         const img = document.createElement("img");
         img.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg";
         img.alt = "selecionado";
         
         span.appendChild(img);
         span.style.backgroundColor = color;
+        span.style.display = "flex";
         label.appendChild(span);
 
           // Adicionar os elementos ao container
@@ -89,6 +96,7 @@ const checkbox = document.getElementById('real_time');
     outputvermelho.innerHTML = this.value;
     //if(checkbox.checked)cor_atual.style.backgroundColor = `rgb(${this.value*2.55}, ${sliderverde.value*2.55}, ${sliderazul.value*2.55})`;
     dechecked();
+    muda_cor_nova();
     //var xhttp = new XMLHttpRequest();
     //xhttp.open("GET", "vermelho_update?vermelho="+this.value, false);
     //xhttp.send();  
@@ -96,22 +104,24 @@ const checkbox = document.getElementById('real_time');
 
   sliderverde.oninput = function() {
     outputverde.innerHTML = this.value;
-    const cor_atual = document.getElementById('cor-nova');
-    cor_atual.style.backgroundColor = `rgb(${slidervermelho.value*2.55}, ${this.value*2.55}, ${sliderazul.value*2.55})`;
+    //const cor_atual = document.getElementById('cor-nova');
+    //cor_atual.style.backgroundColor = `rgb(${slidervermelho.value*2.55}, ${this.value*2.55}, ${sliderazul.value*2.55})`;
     dechecked();
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "verde_update?verde="+this.value, false);
-    xhttp.send();
+    muda_cor_nova();
+    //var xhttp = new XMLHttpRequest();
+    //xhttp.open("GET", "verde_update?verde="+this.value, false);
+    //xhttp.send();
   }
 
   sliderazul.oninput = function() {
     outputazul.innerHTML = this.value;
-    const cor_atual = document.getElementById('cor-nova ');
-    cor_atual.style.backgroundColor = `rgb(${slidervermelho.value*2.55}, ${sliderverde.value*2.55}, ${this.value*2.55})`;
+    //const cor_atual = document.getElementById('cor-nova ');
+    //cor_atual.style.backgroundColor = `rgb(${slidervermelho.value*2.55}, ${sliderverde.value*2.55}, ${this.value*2.55})`;
     dechecked();
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "azul_update?azul="+this.value, false);
-    xhttp.send();
+    muda_cor_nova();
+    //var xhttp = new XMLHttpRequest();
+    //xhttp.open("GET", "azul_update?azul="+this.value, false);
+    //xhttp.send();
   }
 
   const colorWheel = document.getElementById('colorWheel');
@@ -284,7 +294,12 @@ const checkbox = document.getElementById('real_time');
   };
 
   function muda_cor_nova(){
-
+    red = Math.round(slidervermelho.value*2.55);
+    green = Math.round(sliderverde.value*2.55);
+    blue = Math.round(sliderazul.value*2.55);
+    cor = `rgb(${red}, ${green}, ${blue})`;
+    console.log(`cor = ${cor}`)
+    cor_nova.style.backgroundColor = cor;
   }
 
   function atualiza_cor(){
